735a143adac9053a52c4bc88c70f3dc3d8325028
ToruNiina <niina.toru.68u@gmail.com>
2019-01-27 13:34:21 +0900

    add camera position to screen

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,7 @@ mod background;
 
 fn main() {
     let screen = screen::Screen::new(
+        vector::Vector3::new( 0.0,  0.0,  0.0),
         vector::Vector3::new(-2.0, -1.0, -1.0),
         vector::Vector3::new( 4.0,  0.0,  0.0),
         vector::Vector3::new( 0.0,  2.0,  0.0),
--- src/screen.rs
+++ src/screen.rs
@@ -3,6 +3,7 @@ use crate::image::{Color, Image};
 use crate::background::Background;
 
 pub struct Screen<B: Background> {
+    camera:     Vector3,
     lower_left: Vector3,
     horizontal: Vector3,
     vertical:   Vector3,
@@ -12,10 +13,15 @@ pub struct Screen<B: Background> {
 }
 
 impl<B: Background> Screen<B> {
-    pub fn new(lower_left: Vector3, horizontal: Vector3, vertical:   Vector3,
-               width:      usize,   height:     usize,   background: B)
+    pub fn new(camera:     Vector3,
+               lower_left: Vector3,
+               horizontal: Vector3,
+               vertical:   Vector3,
+               width:      usize,
+               height:     usize,
+               background: B)
         -> Screen<B> {
-        Screen{lower_left, horizontal, vertical, width, height, background}
+        Screen{camera, lower_left, horizontal, vertical, width, height, background}
     }
 
     pub fn image(&self) -> Image {
